bc343c09bb2999828fcfe1b18520c8c8d63e6c41
MakerSquare Student <jdaudier@outlook.com>
2013-06-25 16:36:41 -0500

    With consecutive modifiers now

--- twitter_use.rb
+++ twitter_use.rb
@@ -8,48 +8,34 @@ Twitter.configure do |config|
 end
 
 class Dates
+  
+  # Create an array of dates included in queries which will be crossed against the tweet_counter loop
+  # to count tweest on a per-day basis
 
   def self.week_dates
     i = 0
     @@dates = []
     while i < 8
-      
       @@dates <<(Time.now - i*86400).strftime("%Y-%m-%d")
       i += 1
     end
-    #@dates.reverse! first item in array is the lowest date
     @@dates
   end
 
 end
 
+
+
 class Tweeter
-  attr_accessor :total_tweets, :handle, :dates
+  attr_accessor :total_tweets
 
-  @@end_date = '2013-06-24' #delete for later?
-  @@start_date = '2012-06-03' #will populate later (Time.now-86400).strftime("%Y-%m-%d")
-  @@Tweeters = []
-  @@Points = []
+  @@tweeters = []
+  @@points = []
   @tweet_point = 1
-   #will use to calculate consecutiveness
 
-# Create an array of dates included in queries which will be crossed against the tweet_counter loop
-# to count tweest on a per-day basis
-  def week_dates
-    i = 0
-    @dates = []
-    while i < 8
-      @dates <<(Time.now - i*86400).strftime("%Y-%m-%d")
-      i += 1
-    end
-    #@dates.reverse! first item in array is the lowest date
-    @dates
-  end
-
-  def initialize(handle)
-    @handle = handle
+  def initialize
     @total_tweets = 0 #total number of tweets pulled for a handle using API
-    @@Tweeters << @handle
+    @@tweeters << @handle
   end
 
   #count tweets -- FINISHED
@@ -60,29 +46,49 @@ class Tweeter
     @tweets_per_day = [] #array contains number of tweets for each day of the week by index 0 being today and 6 being start of the week
     @total_tweets = 0
     until i > (Dates.week_dates.length-2) #until the last item in array, which we need to pull today's tweet
-      @tweets << Twitter.search("from:#{handle}", :count => 15, :until => Dates.week_dates[i], :since => Dates.week_dates[i+1]).results.map do |status|
+      @tweets << Twitter.search("from:#{handle}", :count => 5, :until => Dates.week_dates[i], :since => Dates.week_dates[i+1]).results.map do |status|
         "#{status.from_user}/ #{status.text} / #{Dates.week_dates[i]}" #if need to test, push this into array and puts it later
-        # sleep(1)
       end
-      @tweets_per_day.push(@tweets[i].length) #storing on a given day how many tweets were tweeted
+      @tweets_per_day << @tweets[i].length #storing on a given day how many tweets were tweeted to calculate consecutiveness
       @total_tweets += @tweets[i].length
       i +=1
     end
-      @total_tweets
-
+      puts @total_tweets
   end
 
-  def self.tweeter_points
-    @@Points = @@Tweeters.map do |tweeter|
-      tweet_counter(tweeter)*@tweet_point
+  #calculate points for original tweets only
+  def tweet_points
+    @points_per_day = @tweets_per_day.map do |tweets|
+      tweets*3.1
     end
+    puts @points_per_day.inspect
   end
 
-  
-
+  def consec_points
+    #example of array -> [0,1,2,3,1,4,0]
+    #i = 0
+    index = 0 
+    i = 1
+    @consec_modified = @points_per_day.map do |point|
+      if index > 0
+        if @points_per_day[index] > 0 && @points_per_day[index-1] > 0
+          i+=0.5
+        else
+          i = 1
+        end
+      else 
+        i = 1
+      end
+      index += 1
+      point*i
+    end
+    puts @consec_modified.inspect
+  end
 end
 
-shannon = Tweeter.new('s_byrne')
+shannon = Tweeter.new
 shannon.tweet_counter('s_byrne')
+shannon.tweet_points
+shannon.consec_points
 
 # youssif.week_dates
