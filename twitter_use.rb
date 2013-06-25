271a184874eb192a554cf07eb06281ec08b128ab
MakerSquare Student <jdaudier@outlook.com>
2013-06-25 15:06:58 -0500

    It's working

--- twitter_use.rb
+++ twitter_use.rb
@@ -7,15 +7,33 @@ Twitter.configure do |config|
   config.oauth_token_secret = ENV['YOUR_OAUTH_TOKEN_SECRET']
 end
 
-class Tweeter
-  attr_accessor :total_tweets, :handle
+class Dates
 
-  @@end_date = '2013-06-24' #must be formatted as YYYY-MM-DD
+  def self.week_dates
+    i = 0
+    @@dates = []
+    while i < 8
+      
+      @@dates <<(Time.now - i*86400).strftime("%Y-%m-%d")
+      i += 1
+    end
+    #@dates.reverse! first item in array is the lowest date
+    @@dates
+  end
+
+end
+
+class Tweeter
+  attr_accessor :total_tweets, :handle, :dates
+
+  @@end_date = '2013-06-24' #delete for later?
   @@start_date = '2012-06-03' #will populate later (Time.now-86400).strftime("%Y-%m-%d")
   @@Tweeters = []
   @@Points = []
   @tweet_point = 1
 
+# Create an array of dates included in queries which will be crossed against the tweet_counter loop
+# to count tweest on a per-day basis
   def week_dates
     i = 0
     @dates = []
@@ -23,7 +41,8 @@ class Tweeter
       @dates <<(Time.now - i*86400).strftime("%Y-%m-%d")
       i += 1
     end
-    @dates.reverse! #first item in array is the lowest date
+    #@dates.reverse! first item in array is the lowest date
+    @dates
   end
 
   def initialize(handle)
@@ -34,17 +53,22 @@ class Tweeter
 
   #count tweets
   def tweet_counter(handle)
-    week_dates
+    Dates.week_dates
     i = 0
-    until i < (@dates.length-1) #until the last item in array, which we need to pull today's tweet
-      tweets = Twitter.search("from:#{handle}", :count => 20000, :until => @dates[i+1], :since => @dates[i]).results.map do |status|
-        "#{status.from_user} / #{status.text} / #{@dates[]}"
-        i +=1
+    @tweets = []
+    until i > (Dates.week_dates.length-2) #until the last item in array, which we need to pull today's tweet
+      @tweets << Twitter.search("from:#{handle}", :count => 15, :until => Dates.week_dates[i], :since => Dates.week_dates[i+1]).results.map do |status|
+        "#{status.from_user}/ #{status.text} / #{Dates.week_dates[i]}"
+        sleep(1)
       end
-      tweets.count
-      puts tweets.count
-      puts tweets
+      puts @tweets[i].length
+      i +=1
     end
+      puts "IT'S WORKING!!!!"
+      @tweets.length
+      puts @tweets.length
+      puts @tweets.inspect
+
   end
 
   def self.tweeter_points
@@ -59,4 +83,4 @@ end
 
 youssif = Tweeter.new('s_byrne')
 youssif.tweet_counter('s_byrne')
-youssif.week_dates
+# youssif.week_dates
