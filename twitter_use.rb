da5dd65bd28f546c1ec8848e1305957a49870f2e
MakerSquare Student <student@macintosh.att.net>
2013-06-28 14:30:39 -0500

    EVERYTHING FREAKING WORKS

--- twitter_use.rb
+++ twitter_use.rb
@@ -13,24 +13,17 @@ class Email
   @@password = ENV['GMAILPSWD']
 
   def self.send(emails)
-    @flock = Flock.new(["sagarispatel","s_byrne","techpeace","makersquare","youssifwashere","lydiaguarino"])
-    @email_body = @flock.email_format
-
+    flock = Flock.new(["mrshaasha","sagarispatel","s_byrne","techpeace","makersquare","youssifwashere","lydiaguarino","jeremyjboyd"])
+    email_body = flock.email_format
     gmail = Gmail.connect(@@username, @@password)
-      emails.each do |email|
-        gmail.deliver do
-          to email
-          subject "Your Weekly Mother Flockerboard"
-          text_part do
-            body @email_body #this is breaking us!!!!!!! AHHHHH!!!!
-          end
-          html_part do
-            content_type 'text/html; charset=UTF-8'
-            # body @
-          end
-          # add_file "/path/to/some_image.jpg"
-        end
+    emails.each do |recipient|
+      email = gmail.compose do
+        to recipient
+        subject "Mother Flocker Weekly Leaderboard"
+        body email_body
       end
+      email.deliver!
+    end
     gmail.logout
   end
 end
@@ -55,24 +48,19 @@ end
 class Tweeter
   attr_accessor :total_tweets
 
-  @@tweeters = []
-  @@points = []
-  @tweet_point = 1
-
-  def initialize
-    @total_tweets = 0 #total number of tweets pulled for a handle using API
-    @@tweeters << @handle
+  def initialize(dates)
+    @dates=dates
+#total number of tweets pulled for a handle using API
   end
 
   #count tweets -- FINISHED
   def tweet_counter(handle)
-    Dates.week_dates
     i = 0
     @tweets = []
     @tweets_per_day = [] #array contains number of tweets for each day of the week by index 0 being today and 6 being start of the week
     @total_tweets = 0
-    until i > (Dates.week_dates.length-2) #until the last item in array, which we need to pull today's tweet
-      @tweets << Twitter.search("from:#{handle}", :count => 5, :until => Dates.week_dates[i], :since => Dates.week_dates[i+1]).results.map do |status|
+    until i > (@dates.length-2) #until the last item in array, which we need to pull today's tweet
+      @tweets << Twitter.search("from:#{handle}", :count => 5, :until => @dates[i], :since => @dates[i+1]).results.map do |status|
         "#{status.id}"
       end
       @tweets_per_day << @tweets[i].length #storing on a given day how many tweets were tweeted to calculate consecutiveness
@@ -84,13 +72,13 @@ class Tweeter
 
   #mentions
   def mention_counter(handle)
-    @mentions = Twitter.search("to:#{handle}", :count => 200, :until => Dates.week_dates[0], :since => Dates.week_dates[6]).results.count
+    @mentions = Twitter.search("to:#{handle}", :count => 200, :until => @dates[0], :since => @dates[6]).results.count
   end
 
   #retweet counter to assign points
   def retweet_count(handle)
     @retweet_total = 0
-    Twitter.search("from:#{handle} -rt", :count => 2000, :until => Dates.week_dates[0], :since => Dates.week_dates[6]).results.map do |status|
+    Twitter.search("from:#{handle} -rt", :count => 2000, :until => @dates[0], :since => @dates[6]).results.map do |status|
        @retweet_total += status.retweet_count
     end
     @retweet_total
@@ -176,12 +164,13 @@ end
 class Flock
   def initialize(array)
     @array = array
+    @@dates = Dates.week_dates
   end
 
   def user_gen
     @flock_array = []
     @array.each do |handle|
-      @flock_array << Tweeter.new.total_points(handle)
+      @flock_array << Tweeter.new(@@dates).total_points(handle)
     end
     @flock_array.sort_by! {|tweeter| tweeter[:total_points]}.reverse!
     @flock_array
@@ -189,18 +178,27 @@ class Flock
 
   def email_format
       user_gen
-      puts "The Mother Flocker Weekly Leaderboard"
-      puts ""
-      puts "@#{@flock_array.first[:handle]} is the Mother Flocker of the week!"
-      puts ""
+      @email_output = "The Mother Flocker Weekly Leaderboard
+
+@#{@flock_array.first[:handle]} is the Mother Flocker of the week!
+
+      "  
     @flock_array.each do |tweeter|
-      puts "@#{tweeter[:handle]} | TOTAL POINTS: #{tweeter[:total_points]} | TOTAL RETWEETS: #{tweeter[:retweet_points]/2} | TOTAL MENTIONS: #{tweeter[:mention_points]/2} | TOTAL TWEETS: #{tweeter[:tweet_points]}" 
-      puts ""
+      @email_output += "@#{tweeter[:handle]} | TOTAL POINTS: #{tweeter[:total_points]} | TOTAL RETWEETS: #{tweeter[:retweet_points]/2} | TOTAL MENTIONS: #{tweeter[:mention_points]/2} | TOTAL TWEETS: #{tweeter[:tweet_points]}
+      
+      "
     end
+    # puts @email_output
+    @email_output
   end
 
 end
 
-Email.send(['abdulyoussif@gmail.com'])
+# flock = Flock.new(['sagarispatel', 'lydiaguarino', 'youssifwashere'])
+# flock.email_format
+
+
+Email.send(['lydiahguarino@gmail.com','boydjj@gmail.com','abdulyoussif@gmail.com'])
+
 
 
